757de71241a95f3ff4ea39b7f7967c744fb67fbf
cute1baby <1138724604@qq.com>
2019-06-29 16:37:08 +0800

    first update

---
+++ mvvm.js
@@ -0,0 +1,227 @@
+/*构造一个Mvvm函数*/
+function Mvvm(options = {}) {
+    // 将所有属性挂载到了$options
+    this.$options = options;
+    // this._data 这里也和Vue一样
+    let data = this._data = this.$options.data;
+
+    // 数据劫持
+    observe(data);
+
+    /**
+     * *数据代理
+     */
+    for (let key in data) {
+        Object.defineProperty(this, key, {
+            configurable: true,
+            get() {
+                return this._data[key];     // 如this.a = {b: 1}
+            },
+            set(newVal) {
+                this._data[key] = newVal;
+            }
+        });
+    }
+
+    /** 初始化computed,将this指向实例**/
+    initComputed.call(this);     
+
+    /*数据编译*/
+    new Compile(options.el, this);
+
+    // 所有事情处理好后执行mounted钩子函数。这就实现了mounted钩子函数 ？？？
+    options.mounted && options.mounted.call(this); 
+}
+
+
+/************************************数据劫持开始******************************************/
+
+/*
+ * Observe：数据劫持的主要逻辑
+ * observe：创建一个函数，无需重复实例化Observe
+ */
+function Observe(data) {
+    // 数据劫持就是通过递归和循环给对象所有属性增加get,set
+    for (let key in data) {     // 把data属性通过defineProperty的方式定义属性
+        let dep = new Dep();
+        let val = data[key];
+        observe(val);   // 递归继续向下找，实现深度的数据劫持
+        Object.defineProperty(data, key, {
+            configurable: true, //该属性是否可删除可配置
+            get() {
+                Dep.target && dep.addSub(Dep.target);   // 将watcher添加到订阅事件中 [watcher]
+                return val;
+            },
+            set(newVal) {   // 更改值的时候
+                if (val === newVal) {   // 设置的值和以前值一样就不理它
+                    return;
+                }
+                val = newVal;   // 如果以后再获取值(get)的时候，将刚才设置的值再返回去
+                observe(newVal);    //当设置为新值后，需要把新值也增加Object.defineProperty
+                dep.notify();   // 让所有watcher的update方法执行即可
+            }
+        });
+    }
+}
+
+// 不用每次调用都写个new，方便递归调用
+function observe(data) {
+    // 如果不是对象的话就直接return掉
+    // 防止递归溢出
+    if (!data || typeof data !== 'object') return;
+    return new Observe(data);
+}
+
+/************************************数据劫持结束******************************************/
+
+
+
+
+/************************************数据编译开始******************************************/
+// 创建Compile构造函数
+function Compile(el, vm) {
+    // 将el挂载到实例上方便调用
+    vm.$el = document.querySelector(el);
+    // 将所有的一级节点添加到文档碎片中。   使用文档碎片中，节省开销
+    let fragment = document.createDocumentFragment();
+    while (child = vm.$el.firstChild) {
+        fragment.appendChild(child);    // 此时将el中的内容放入内存中
+    }
+
+    // 对el里面的内容进行替换
+    function replace(frag) {
+        Array.from(frag.childNodes).forEach(node => {
+            let txt = node.textContent;
+            let reg = /\{\{(.*?)\}\}/g;   // 正则匹配{{}}
+
+            if (node.nodeType === 3 && reg.test(txt)) { // 即是文本节点又有大括号的情况{{}}
+                // console.log(RegExp.$1); // 匹配到的第一个分组 如： a.b, c
+                let arr = RegExp.$1.split('.');
+                let val = vm;
+                arr.forEach(key => {
+                    val = val[key];     // 如this.a.b
+                });
+                // 赋值给txt去除一下首尾空格
+                node.textContent = txt.replace(reg, val).trim();
+
+                // 【新增】实例化监视者，并设定回调函数为DOM节点替换成新值。
+                new Watcher(vm, RegExp.$1, newVal => {
+                    node.textContent = txt.replace(reg, newVal).trim();
+                });
+            }
+
+
+
+            /****************双向数据绑定开始******************/
+            if (node.nodeType === 1) {  // 元素节点
+                let nodeAttr = node.attributes; // 获取dom上的所有属性,是个类数组
+                Array.from(nodeAttr).forEach(attr => {
+                    let name = attr.name;   // v-model  type
+                    let exp = attr.value;   // c        text
+                    if (name.includes('v-')){
+                        node.value = vm[exp];   // this.c 为 2
+                    }
+                    // 监听变化【为啥要写，没明白】
+                    new Watcher(vm, exp, function(newVal) {
+                        node.value = newVal;   // 当watcher触发时会自动将内容放进输入框中
+                    });
+
+                    node.addEventListener('input', e => {
+                        let newVal = e.target.value;
+                        // 相当于给this.c赋了一个新值
+                        // 而值的改变会调用set，set中又会调用notify，notify中调用watcher的update方法实现了更新
+                        vm[exp] = newVal;
+                    });
+                });
+            }
+            /****************双向数据绑定结束******************/
+
+
+            // 如果还有子节点，继续递归replace
+            if (node.childNodes && node.childNodes.length) {
+                replace(node);
+            }
+        });
+    }
+
+    replace(fragment);  // 替换内容
+
+    vm.$el.appendChild(fragment);   // 再将文档碎片放入el中
+}
+
+/************************************数据编译结束******************************************/
+
+
+
+
+/************************************发布订阅开始******************************************/
+// 发布订阅模式  订阅和发布 如[fn1, fn2, fn3]
+function Dep() {
+    // 一个数组(存放函数的事件池)
+    this.subs = [];
+}
+Dep.prototype = {
+    /*addSub是属于订阅*/
+    addSub(sub) {
+        this.subs.push(sub);
+    },
+    notify() {
+        // 绑定的方法，每个sub（其实是Watch的实例）都执行update方法
+        this.subs.forEach(sub => sub.update());
+    }
+};
+
+
+
+// 通过Watcher这个类创建的实例，都拥有update方法，用来更新视图层下的节点
+function Watcher(vm, exp, fn) {
+    this.fn = fn;   // 将fn放到实例上
+
+    // 【新增】这一块的Dep.target个人认为是难点
+    this.vm = vm;
+    this.exp = exp;
+
+    Dep.target = this;
+    let arr = exp.split('.');
+    let val = vm;
+    arr.forEach(key => {    // 取值
+       val = val[key];     // 获取到this.a.b，默认就会调用get方法
+    });
+    Dep.target = null;
+}
+/*update是属于发布*/
+Watcher.prototype.update = function() {
+    let arr = this.exp.split('.');
+    let val = this.vm;
+    arr.forEach(key => {
+        val = val[key];   // 通过get获取到新的值
+    });
+    this.fn(val);   // 将每次拿到的新值去替换{{}}的内容即可
+};
+
+
+/************************************发布订阅结束******************************************/
+
+
+
+/*******************************computed(计算属性)开始*************************************/
+
+function initComputed() {
+    let vm = this;
+    let computed = this.$options.computed;  // 从options上拿到computed属性   {sum: ƒ, noop: ƒ}
+    // 得到的都是对象的key可以通过Object.keys转化为数组
+    Object.keys(computed).forEach(key => {  // key就是sum,noop
+        Object.defineProperty(vm, key, {
+            // 这里判断是computed里的key是对象还是函数
+            // 如果是函数直接就会调get方法
+            // 如果是对象的话，手动调一下get方法即可
+            // 如： sum() {return this.a + this.b;},他们获取a和b的值就会调用get方法
+            // 所以不需要new Watcher去监听变化了
+            get: typeof computed[key] === 'function' ? 
+                 computed[key] : computed[key].get,
+            set() {}
+        });
+    });
+}
+
+/*******************************computed(计算属性)结束*************************************/
---
+++ mvvm.md
@@ -0,0 +1,77 @@
+Object.defineProperty()    给对象定义和修改属性的方法。
+
+
+### 数据劫持
+为什么要做数据劫持
+> 观察对象和深度响应，因为给新对象定义属性或者修改属性时会给这个新对象增加defineProperty
+> 使用的数据属性都需要添加get和set，
+
+数据劫持是这里的Observe函数。
+
+函数做了什么事情？
+	把data中的属性进行循环遍历，给每一个属性都添加Object.defineProperty方法。如果属性值仍是对象，则递归，继续给子类key添加Object.defineProperty方法。
+
+
+
+
+### 数据代理
+数据代理的作用是将mvvm.a代替mvvm.data.a的写法。
+做法是：遍历data，给Mvvm的实例mvvm的每一个属性添加Object.defineProperty方法。并且在的getter中返回this._data[key]，在setter中this._data[key] = newVal。这样就做到了mvvm.data.a的值通过mvvm.a就能取得。
+
+
+
+
+
+### 数据编译
+
+数据编译是这里的Compile函数
+
+使用createDocumentFragment创建文档碎片fragment，并且将根元素下的一级元素存储到文档碎片数组中。将fragment作为参数传入替换函数replace函数中。并且将碎片文档插入到body后面。
+
+replace函数要做的事情就是：通过正则匹配的方式将视图模板中的文本节点和元素节点的内容，将对应key替换成data中对应的属性值。
+
+replace函数的具体逻辑是：
+找到根目录el下的所有一级元素节点，将其遍历。匹配到文本元素且有双大括号情况下，获取正则匹配的第一个分组且根据"."进行分割，获取属性数组，使用val = val[key]的方式循环取值，再将节点的内容替换成val。如视图模板中对应的是a.b.c,那么循环取值的val值就是data.a.b.c，再通过正则的replace替换。
+
+如果一级元素节点下面还有其他节点，那么递归编译。
+
+
+/**************************************总结开始******************************************/
+
+1、上面的三个部分：数据劫持、数据代理和数据编译已经可以正常的将data中的数据对应到视图模板的显示出来了。
+
+2、接下来的【发布订阅】和【数据更新视图】两个部分作用是：数据更新后页面重新渲染
+
+/**************************************总结结束******************************************/
+
+
+### 发布订阅
+发布订阅主要靠的就是数组关系this.subs。用来装观察者Watcher的实例
+
+订阅就是将函数放入数组，对应的是Dep实例的addSub.
+
+发布就是让数组里的函数执行，对应的是Dep实例的notify.
+
+
+
+
+### 数据更新视图
+这一块做的事情比较复杂。分成下面几部分：
+1、在replace中添加new Watcher(vm, RegExp.$1, newVal => {
+	node.textContent = txt.replace(reg, newVal).trim();   
+})
+2、Watcher构造函数添加vm、exp等参数，并且将当前的实例对象赋值给Dep.target。【且获取例如this.a.b的值来触发Observer中的get方法。】获取后立即将Dep.target赋值为null.
+3、在Observer函数上添加get方法，Dep.target && dep.addSub(Dep.target),目的是将watcher添加到事件订阅中;并且在setter中添加dep.notify();
+4、在Watcher的原型update方法上重新获取新的val，且执行新值的回调函数。
+
+
+### 双向数据绑定
+1、在replace函数上添加当node.nodeType为1的情况下，即元素节点。先获取该dom上的所有属性，然后遍历到"v-"属性的时候，将新值赋值给当前节点的value。同时监听input事件的变化，并且将输入的新值赋值给data的属性。
+
+
+
+
+### computed和mounted的使用
+执行的时候在MVVM的回调函数设置：initComputed.call(this); 和options.mounted.call(this); 
+
+initComputed的逻辑是：获取options.computed中的keys并且遍历，使用Object.defineProperty()来定义每一个key上的属性，添加getter方法如果属性值是函数类型，则返回computed[key],是对象则返回computed[key].get,就会触发Observer上的属性的监听相应。
